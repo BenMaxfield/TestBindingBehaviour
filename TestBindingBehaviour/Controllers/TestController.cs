82cb28377d116f5b69b2cb03d7f8d43aee3421a9
BenMaxfield <mxktm1@hotmail.com>
2017-05-29 09:25:10 +0100

    Initial Checkin

---
+++ TestBindingBehaviour/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestBindingBehaviour.Models;
+
+namespace TestBindingBehaviour.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index(string username)
+        {
+            string name = null;
+            string bio = null;
+
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                name = "Not set";
+                bio = "No Username Passed";
+            }
+            else
+            {
+                name = "username";
+                bio = "Username was Passed";
+
+            }
+            var model = new TestModel()
+            {
+                Bio = bio,
+                Username = name
+            };
+            return View(model);
+        }
+    }
+}
---
+++ TestBindingBehaviour/Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestBindingBehaviour.Models
+{
+    public class TestModel
+    {
+        public string Username { get; set; }
+        public string Bio { get; set; }
+    }
+}
